1687285c1265e12fea3fba669346cc6b2187d165
Dario Amade <dario_isd@outlook.com>
2021-04-23 09:55:53 +0100

    remve map in small screns

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9599,7 +9599,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59808" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51440" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
